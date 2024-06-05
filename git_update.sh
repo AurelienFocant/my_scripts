3333eecc8ef6723eb5d2b83991d019d9fb32a579
Aurelien <afocant@student.s19.be>
2024-06-05 23:18:55 +0200

    better version of git_update script, better formatting and a few additional infos

--- git_update.sh
+++ git_update.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 for dir in */; do
+	echo ""
 	echo "-----------------------------------------------"
 	if [ -d $dir/.git ]; then
-		echo "Entering $dir"
+		echo "Entering $PWD/$dir"
 		cd $dir
 		echo "Fetching data from remote"
 		git fetch -v
 		echo ""
 		echo "Checking git status"
 		git status -v
-		echo "Exiting $dir"
 		cd ..
+		echo "Exiting $PWD/$dir"
 	else
 		echo "Not a GIT directory: $dir"
 	fi
 	echo "-----------------------------------------------"
 done
 
-echo ""
 echo ""

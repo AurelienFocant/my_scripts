d414dc98f5cc75b1719b6e4a677cedd59efa63bf
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-24 17:44:58 +0200

    corrected bug on |sed -i ''| command, was adding a '=' to the name of files it would midify

--- get_ctags.sh
+++ get_ctags.sh
@@ -7,9 +7,9 @@ if [ -d ./src ]; then
 	[ -f tags ] && rm tags
 
 	find .. -type f \( -name "*.c" -o -name "*.h" \) \
-		-exec sed -i="" 's/# define/#define/g' {} \; \
+		-exec sed -i "" 's/# define/#define/g' {} \; \
 		-exec ctags -adtw {} \; \
-		-exec sed -i="" 's/#define/# define/g' {} \; \
+		-exec sed -i "" 's/#define/# define/g' {} \; \
 	&& \
 	echo "Tags file created"
 

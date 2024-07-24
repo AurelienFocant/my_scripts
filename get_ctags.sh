4cf3ccbb132143fbde68421de183b6180d107122
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-24 15:25:53 +0200

    slightly refined ctags script

--- get_ctags.sh
+++ get_ctags.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-if [ -d ./misc ] && [ -d ./src ]; then
+if [ -d ./src ]; then
+
+	if [ ! -d ./misc ]; then
+		mkdir -p misc
+	fi
+
 	cd ./misc
 	ctags ../src/*.c
-	ctags -a ../src/*/*.c
+	ctags -a ../src/*/*.c 2>/dev/null
+	echo "Tags file created"
 	cd ..
+
 else
 	echo "You are not in a valid project directory"
 fi

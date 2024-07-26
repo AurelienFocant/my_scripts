7a5ba9a1f2e79b360ea1176399bc8df2e26dfb6d
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-26 18:45:36 +0200

    update of prototypes.sh to create and append the header file and its dir

---
+++ my_prototypes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+src_dir="./src"
+
+if [ -d  $src_dir ]; then 
+
+	[ ! -d includes ] && mkdir includes
+	echo "${PWD##*/}" | sed -E 's/[1-9]_//g' | awk '{print "#include " "\"" $0 ".h\""}' >includes/prototypes.h
+
+	find $src_dir -type f -name "*.c" \
+		-exec sh -c \
+		'echo "\n/*----------------  ${1##*/}  ---------------*/"; \
+		grep -E "^[[:space:]]*([a-zA-Z_*]+[[:space:]]+){1,2}[a-zA-Z_*]+\([^\)]*\)" $1 \
+		| grep -vE "^[[:space:]]*static[[:space:]]+" \
+		| sed "s/$/;/";' \
+		_ {} \; \
+	   	>>includes/prototypes.h 2>/dev/null
+
+else
+	echo "You are not in a valid project directory"
+fi

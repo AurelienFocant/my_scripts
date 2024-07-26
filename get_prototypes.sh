b7f92f11715ac2fd7293ec7ccd3536a45c27c376
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-26 17:31:31 +0200

    deleted colors from get_prototype script, was messing with *.h files

--- get_prototypes.sh
+++ get_prototypes.sh
@@ -9,7 +9,7 @@ function prototype() {
     if [ -z "$dir" ]; then
         dir="."
     fi
-    find "$dir" -type f -name "*.c" -exec sh -c 'echo "\033[1;32m\n/*----------------  ${1##*/}  ---------------*/\033[0m"; grep -E "^[[:space:]]*([a-zA-Z_*]+[[:space:]]+){1,2}[a-zA-Z_*]+\([^\)]*\)" $1 | grep -vE "^[[:space:]]*static[[:space:]]+" | sed "s/$/;/";' _ {} \;
+    find "$dir" -type f -name "*.c" -exec sh -c 'echo "\n/*----------------  ${1##*/}  ---------------*/"; grep -E "^[[:space:]]*([a-zA-Z_*]+[[:space:]]+){1,2}[a-zA-Z_*]+\([^\)]*\)" $1 | grep -vE "^[[:space:]]*static[[:space:]]+" | sed "s/$/;/";' _ {} \;
 }
 
 prototype

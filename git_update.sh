18a3f4e1194d7baa266c6d99b3148330b6883184
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-30 14:18:47 +0200

    updated git update script to get output in red in case of not up to date repo

--- git_update.sh
+++ git_update.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+red='\033[0;31m'
+white='\033[0;37m'
+
 # Loop through each subdirectory in the current directory
 for dir in */; do
 	# Check if the directory contains a .git folder
@@ -10,12 +13,15 @@ for dir in */; do
 		cd $dir
 
 		#Execute git fetch and git status
-		echo "Fetching data from remote..."
-		git fetch -v
+		#echo "Fetching data from remote..."
+		git fetch
 		echo ""
 
 		echo "Checking git status..."
 		git status -v
+		if git status -v | grep --quiet "behind\|ahead"; then
+			echo -e "${red}You are not up to date${white}"
+		fi
 
 		#Return to the parent directory
 		cd ..

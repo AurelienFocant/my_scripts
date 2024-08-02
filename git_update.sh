5dda3f848309a63e9c08e6909be62cdbda25b5ba
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-08-02 11:35:30 +0200

    better gut script for when in a git directory already

--- git_update.sh
+++ git_update.sh
@@ -3,33 +3,42 @@
 red='\033[0;31m'
 white='\033[0;37m'
 
-# Loop through each subdirectory in the current directory
-for dir in */; do
-	# Check if the directory contains a .git folder
-	if [ -d $dir/.git ]; then
-		echo ""
-		echo "-----------------------------------------------"
-		echo "Entering $PWD/$dir"
-		cd $dir
+git_fetch_and_status () {
+	echo ""
+	echo "-----------------------------------------------"
+	echo "$PWD"
+	echo "Fetching changes from remote"
+	#Execute git fetch and git status
+	#echo "Fetching data from remote..."
+	git fetch
+	echo ""
 
-		#Execute git fetch and git status
-		#echo "Fetching data from remote..."
-		git fetch
-		echo ""
-
-		echo "Checking git status..."
-		git status -v
-		if git status -v | grep --quiet "behind\|ahead"; then
-			echo -e "${red}You are not up to date${white}"
-		fi
-
-		#Return to the parent directory
-		cd ..
-		echo "Exiting $PWD/$dir"
-		#else
-		#	echo "Not a GIT directory: $dir"
-		echo "-----------------------------------------------"
+	echo "Checking git status..."
+	git status -v
+	if git status -v | grep --quiet "behind\|ahead"; then
+		echo -e "${red}You are not up to date${white}"
 	fi
-done
+
+	#Return to the parent directory
+	echo "$PWD"
+	#else
+	#	echo "Not a GIT directory: $dir"
+	echo "-----------------------------------------------"
+
+}
+
+if [ -d .git ]; then
+	git_fetch_and_status
+else
+	# Loop through each subdirectory in the current directory
+	for dir in */; do
+		# Check if the directory contains a .git folder
+		if [ -d $dir/.git ]; then
+		cd $dir
+		git_fetch_and_status
+		cd ..
+		fi
+	done
+fi
 
 echo ""

1ba9ec7aefa88ab0de9a22c6fefa1016eab010b6
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-08-23 21:12:31 +0200

    re-organizing my scripts but still very messy

--- get_ctags.sh
+++ get_ctags.sh
@@ -1,22 +1,27 @@
 #! /bin/bash
 
-if [ -d ./src ]; then
+src_dir="$1"
+if [ -z "$src_dir" ]; then
+	src_dir="./src"
+fi
+
+if [ -d $src_dir ]; then
 
 	[ ! -d ./misc ] && mkdir -p misc
 	cd ./misc
 	[ -f tags ] && rm tags
 
-if uname | grep --quiet Linux; then
-	find -L .. -type f \( -name "*.c" -o -name "*.h" \) \
-		-exec sed -i 's/# define/#define/g' {} \; \
-		-exec ctags -a {} \; \
-		-exec sed -i 's/#define/# define/g' {} \;
-else
-	find -L .. -type f \( -name "*.c" -o -name "*.h" \) \
-		-exec sed -i "" 's/# define/#define/g' {} \; \
-		-exec ctags -adtw {} \; \
-		-exec sed -i "" 's/#define/# define/g' {} \;
-fi
+	if uname | grep --quiet Linux; then
+		find -L .. -type f \( -name "*.c" -o -name "*.h" \) \
+			-exec sed -i 's/# define/#define/g' {} \; \
+			-exec ctags -a {} \; \
+			-exec sed -i 's/#define/# define/g' {} \;
+	else
+		find -L .. -type f \( -name "*.c" -o -name "*.h" \) \
+			-exec sed -i "" 's/# define/#define/g' {} \; \
+			-exec ctags -adtw {} \; \
+			-exec sed -i "" 's/#define/# define/g' {} \;
+	fi
 
 	sort tags -o tags
 
--- my_prototypes.sh
+++ my_prototypes.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 project=$(echo "${PWD##*/}" | sed -E 's/[1-9]_//g')
-src_dir="./src"
+#src_dir="./src"
 inc_dir="./includes"
 header_file="${inc_dir}/prototypes_${project}"
+
+src_dir="$1"
+if [ -z "$dir" ]; then
+	src_dir="./src"
+fi
 
 if [ -d  $src_dir ]; then 
 
@@ -18,32 +23,8 @@ if [ -d  $src_dir ]; then
 		| grep -vE "[[:space:]]*main\(" \
 		| sed "s/$/;/";' \
 		_ {} \; \
-	   	>>${header_file}.h 2>/dev/null
+		>>${header_file}.h 2>/dev/null
 
 else
 	echo "There is no src directory"
-fi
-
-project=$(echo "${PWD##*/}" | sed -E 's/[1-9]_//g')
-src_dir="./src_bonus"
-inc_dir="./includes_bonus"
-header_file="${inc_dir}/prototypes_${project}"
-
-if [ -d  $src_dir ]; then 
-
-	[ ! -d ${inc_dir} ] && mkdir ${inc_dir}
-	echo ${project} | awk '{print "#include " "\"" $0 ".h\""}' >${header_file}_bonus.h
-
-	find $src_dir -type f -name "*.c" \
-		-exec sh -c \
-		'echo "\n/*----------------  ${1##*/}  ---------------*/"; \
-		grep -E "^[[:space:]]*([a-zA-Z_*]+[[:space:]]+){1,2}[a-zA-Z_*]+\([^\)]*\)" $1 \
-		| grep -vE "^[[:space:]]*static[[:space:]]+" \
-		| grep -vE "[[:space:]]*main\(" \
-		| sed "s/$/;/";' \
-		_ {} \; \
-	   	>>${header_file}_bonus.h 2>/dev/null
-
-else
-	echo "There is no src_bonus directory"
 fi

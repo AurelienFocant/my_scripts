a29f30b4685730326deb78a15c6c70ad95ed7305
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-09-28 09:19:08 +0200

    changed ctags to use Universal instead of Exuberant ctags

--- get_ctags.sh
+++ get_ctags.sh
@@ -4,19 +4,18 @@ src_dir="${1:-src}"
 inc_dir="${2:-include}"
 libft_dir="${3:-libft}"
 
-
 if [ -d $src_dir ]; then
 
 	[ ! -d ./misc ] && mkdir -p misc
 	cd ./misc
 	[ -f tags ] && rm tags
 
-	if ctags --version 2>/dev/null | grep --quiet Exuberant; then
+	if ctags --version 2>/dev/null | grep --quiet Universal; then
 		find -L ../${inc_dir} ../${src_dir} ../${libft_dir} -type f \( -name "*.c" -o -name "*.h" \) \
 			-exec ctags -a {} \;
 
 	elif ! ctags --version 2>/dev/null; then
-		find -L ../${inc_dir} ../${src_dir} ../${libft_dir}  -type f \( -name "*.c" -o -name "*.h" \) \
+		find -L ../${inc_dir} ../${src_dir} ../${libft_dir} -type f \( -name "*.c" -o -name "*.h" \) \
 			-exec sed -i "" 's/# define/#define/g' {} \; \
 			-exec ctags -adtw {} \; \
 			-exec sed -i "" 's/#define/# define/g' {} \;

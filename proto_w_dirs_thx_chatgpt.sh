67491c6b48d789321bead27e4aac6e1d61aa661c
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-10-02 04:51:55 +0200

    better known issues

--- proto_w_dirs_thx_chatgpt.sh
+++ proto_w_dirs_thx_chatgpt.sh
@@ -5,19 +5,19 @@
 #################################################################################
 #	This script is intented to facilitate prototyping software in C				#
 #	Every time you write a new function, [in a new file],						#
-#	[In a new subdirectory], in your source files directory						#
+#	[In a new subdirectory], in your source_files directory						#
 #	It will update (== create) a header file with all function prototypes.		#
 #	You can now just #include this prototype_file.h in your main header file	#
 #################################################################################
 
 #!/bin/bash
 
-# You can specify the source files directory as 1st argument
+# You can specify the source_files directory as 1st argument
 # If not, it will be called "src" by default
 src_dir="${1:-src}"
 
-# The name of the project is the name of the main directory
-# Bar some numbers that I use for faster shell completion
+# The name of the project is the name of the directory in which you execute the script
+# Bar some numbers that I personally use for faster shell completion
 project=$(echo "${PWD##*/}" | sed -E 's/^[1-9]_//g')
 
 # All header files to be in this directory
@@ -91,15 +91,18 @@ get_prototypes
 # *
 # ******/
 
-# known issue: 
+# known issue:
 	# doesnt work if there's a newline in the function name 
 	# because grep works on a line by line basis
-# known issue: 
-	# does not support embedded subdirectories
-# known issue: 
+# known issue:
 	# in case of existing file with no elligible function
-	# eg file with only main function
-	# the header file will still write those file names
-# known issue: 
+	# eg. a file with only a main function
+	# the script will still write the file name in the header file
+# known issue:
+	# if you want to declare structs in your main header file
+	# you'll have to include the prototype file after those
+	# and 1942 norm does not like this
+	# you'll have to reformat everything before push
+# known issue:
 	# does not tab-align function names according to 1942 norm
 	# ... I'm not good enough at AWK yet ...

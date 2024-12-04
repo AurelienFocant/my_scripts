7cd2a61bda6881c2a0285accee473a7d4b8bb1c7
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-04 00:03:59 -0300

    changed vim Sessions script to use neovim

--- vim/vim_session.sh
+++ vim/vim_session.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-[ -d misc ] && dir="misc" || dir="."
-vim -S $dir/Session.vim -c 'Le | Le'
+# [ -d misc ] && dir="misc" || dir="."
+# vim -S $dir/Session.vim -c 'Le | Le'
+
+nvim -S misc/Session.vim -c "NvimTreeToggle"
